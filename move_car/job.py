e8ddf27d1c3621bf7feb76b5040656c9416c6b5a
camilooob <camilobaq@hotmail.com>
2023-12-31 16:44:09 -0500

    query update for

--- move_car/job.py
+++ move_car/job.py
@@ -1,6 +1,6 @@
 from get_db import execute_query
 
-# Querys generados por datagrip.
+# Querys
 update_ubicacion_people_1 = "UPDATE cityopeen.people SET y = 6 WHERE idp = 2"
 update_ubicacion_people_2 = "UPDATE cityopeen.people SET y = 6 WHERE idp = 1"
 update_query_map_1 = "UPDATE cityopeen.layer_map SET `9` = '=R1O2' WHERE id = 6"
@@ -8,16 +8,11 @@ update_query_map_2 = "UPDATE cityopeen.layer_map SET `9` = 'V' WHERE id = 4"
 update_query_map_3 = "UPDATE cityopeen.layer_map SET `9` = 'V' WHERE id = 5"
 update_query_object_1 = "UPDATE cityopeen.objects SET y = 6 WHERE ido = 1"
 
+query_list = [update_ubicacion_people_1, update_ubicacion_people_2, update_query_map_1, update_query_map_2, update_query_map_3, update_query_object_1]
+for query in query_list:
+    execute_query(query)
 # Ejecutando querys
 
-r = execute_query(update_ubicacion_people_1)
-execute_query(update_ubicacion_people_2)
-execute_query(update_query_map_1)
-execute_query(update_query_map_2)
-execute_query(update_query_map_3)
-execute_query(update_query_object_1)
-
-print(r)
 print("Querys ejecutados correctamente")
 
 
